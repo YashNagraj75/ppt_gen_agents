01502200a46b0c22d6242bc9fe78b37df33728d0
YashNagraj75 <yashnagraj75@gmail.com>
2025-04-10 20:27:13 +0530

    Add agents for content generation

---
+++ agents/content_agents.py
@@ -0,0 +1,37 @@
+from agents_new import client
+
+from agents import Agent, ModelSettings, OpenAIChatCompletionsModel
+
+content_generator = Agent(
+    name="Content Generator",
+    instructions="",
+    model=OpenAIChatCompletionsModel("gemini-2.0-flash-001", openai_client=client),
+    model_settings=ModelSettings(temperature=0.6),
+)
+
+chart_agent = Agent(
+    name="Chart Generator",
+    instructions="",
+    model=OpenAIChatCompletionsModel("gemini-2.0-flash-001", openai_client=client),
+    model_settings=ModelSettings(temperature=0.6),
+)
+
+text_agent = chart_agent.clone(
+    name="Content Generator",
+    instructions="Make content",
+)
+
+table_agent = chart_agent.clone(
+    name="Table Generator",
+    instructions="Generate tables",
+)
+
+image_agent = chart_agent.clone(
+    name="Table Generator",
+    instructions="Generate tables",
+)
+
+media_agent = chart_agent.clone(
+    name="Table Generator",
+    instructions="Generate tables",
+)
