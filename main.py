f7fe84b28400f8c930a347edf487b2a0eb2f6bb3
YashNagraj75 <yashnagraj75@gmail.com>
2025-05-04 12:55:40 +0530

    Add create task to ack immediately

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import base64
 import json
 
@@ -27,5 +28,5 @@ async def create(push: PubSubPush):
         raise HTTPException(status_code=400, detail="`doc_id` missing in payload")
 
     print(f"Received doc_id: {doc_id}")
-    success = await main(doc_id)
-    return {"status": success}
+    asyncio.create_task(main(doc_id))
+    return {"status": "started"}
